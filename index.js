2bd6162d85cbdd4bbc13f13107da87e39276c712
Ruben Schlaen <gschlaen@agro.uba.ar>
2022-04-01 16:55:17 -0300

    sockets server ready

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+// Para leer el archivo .env y establecer las variables de entorno
+require('dotenv').config();
+
+// App de Express
+const app = express();
+
+// Node Server
+// Levanta server con la configuracion seteada en app 
+// y le anexa la aplicacion de sockets
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+require('./sockets/socket');
+
+
+
+// Public path
+const publicPath = path.resolve(__dirname, 'public');
+
+app.use(express.static(publicPath));
+
+
+
+server.listen(process.env.PORT, (err) =>{
+
+    if (err) throw new Error(err);
+
+    console.log('Servidor corriendo en puerto!!!', 3000);
+    
+})
